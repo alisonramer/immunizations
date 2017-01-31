f8e6b1e17daa9d9384c16b48ca286429bd3e7b44
radenska <yana_radenska@yahoo.com>
2017-01-31 01:18:32 -0800

    added index and js files, working on getting school coordinates, added server.js and routes.js

---
+++ public/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var addresses = [];
+var latlng = [];
+
+// $.get('https://data.wa.gov/resource/jyj7-ns6j.json')
+//  .then(data => {
+//    addresses = data.filter(school => school.has_6thgrade === 'Y')
+//   .map(school => { return school.school_name + ',' + school.address + ',' + school.city + ',WA'})
+//    console.log(addresses);
+//  });
