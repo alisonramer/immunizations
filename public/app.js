aa8939839a37918f35e57bd718a5ead96384621a
radenska <yana_radenska@yahoo.com>
2017-02-05 20:28:03 -0800

    added back info windows, polygon highlighting state

--- public/app.js
+++ public/app.js
@@ -25,7 +25,7 @@ var stylesArray =
       'elementType': 'labels.text.fill',
       'stylers': [
         {
-          'color': '#757575'
+          'color': '#212121'
         }
       ]
     },
@@ -42,7 +42,7 @@ var stylesArray =
       'elementType': 'geometry',
       'stylers': [
         {
-          'color': '#757575'
+          'color': '#808B96'
         }
       ]
     },
@@ -86,7 +86,7 @@ var stylesArray =
       'elementType': 'geometry.fill',
       'stylers': [
         {
-          'color': '#2c2c2c'
+          'color': '#373737'
         }
       ]
     },
@@ -95,7 +95,7 @@ var stylesArray =
       'elementType': 'labels.text.fill',
       'stylers': [
         {
-          'color': '#8a8a8a'
+          'color': '#373737'
         }
       ]
     },
@@ -131,7 +131,7 @@ var stylesArray =
       'elementType': 'labels.text.fill',
       'stylers': [
         {
-          'color': '#616161'
+          'color': '#3c3c3c'
         }
       ]
     },
@@ -140,7 +140,7 @@ var stylesArray =
       'elementType': 'labels.text.fill',
       'stylers': [
         {
-          'color': '#757575'
+          'color': '#3c3c3c'
         }
       ]
     },
@@ -167,7 +167,7 @@ var stylesArray =
       'elementType': 'geometry',
       'stylers': [
         {
-          'color': '#212121'
+          'color': '#808B96'
         },
         { 'visibility': 'on'}
       ]
@@ -185,13 +185,78 @@ var mapOptions = {
   }
 }
 
-// var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-// google.maps.event.addDomListener(window, 'resize', function() {
-//   var center = {lat: 47.015177, lng: -119.790176};
-//   google.maps.event.trigger(map, 'resize');
-//   map.setCenter(center);
-// });
+google.maps.event.addDomListener(window, 'resize', function() {
+  var center = {lat: 47.015177, lng: -119.790176};
+  google.maps.event.trigger(map, 'resize');
+  map.setCenter(center);
+});
+
+var washCoords = [
+  {lat: 49.0037, lng: -123.3215},
+  {lat: 49.0037, lng: -117.0346},
+  {lat: 46.4038, lng: -117.0401},
+  {lat: 46.3488, lng: -117.0621},
+  {lat: 46.2445, lng: -116.955},
+  {lat: 46.2046, lng: -116.9632},
+  {lat: 46.1608, lng: -116.9193},
+  {lat: 46.0847, lng: -116.9824},
+  {lat: 45.9989, lng: -116.9165},
+  {lat: 46.0008, lng: -118.9737},
+  {lat: 45.9282, lng: -119.1385},
+  {lat: 45.9397, lng: -119.2676},
+  {lat: 45.9015, lng: -119.4818},
+  {lat: 45.9263, lng: -119.5944},
+  {lat: 45.8613, lng: -119.6658},
+  {lat: 45.7733, lng: -120.1547},
+  {lat: 45.6966, lng: -120.4871},
+  {lat: 45.7522, lng: -120.6107},
+  {lat: 45.6525, lng: -120.9128},
+  {lat: 45.6525, lng: -121.0831},
+  {lat: 45.6025, lng: -121.1792},
+  {lat: 45.6659, lng: -121.2176},
+  {lat: 45.7004, lng: -121.344},
+  {lat: 45.7062, lng: -121.8137},
+  {lat: 45.5429, lng: -122.3108},
+  {lat: 45.6371, lng: -122.753},
+  {lat: 46.0828, lng: -122.904},
+  {lat: 46.1836, lng: -123.1567},
+  {lat: 46.1418, lng: -123.305},
+  {lat: 46.271, lng: -123.4918},
+  {lat: 46.235, lng: -123.8763},
+  {lat: 46.2672, lng: -124.0878},
+  {lat: 46.8752, lng: -124.1455},
+  {lat: 47.2681, lng: -124.2224},
+  {lat: 47.3686, lng: -124.3268},
+  {lat: 47.7209, lng: -124.4257},
+  {lat: 47.8279, lng: -124.5135},
+  {lat: 47.9053, lng: -124.6454},
+  {lat: 48.1771, lng: -124.7388,},
+  {lat: 48.4984, lng: -124.7827},
+  {lat: 48.4966, lng: -124.7292},
+  {lat: 48.296, lng: -124.0137},
+  {lat: 48.2247, lng: -123.5426},
+  {lat: 48.285, lng: -123.246},
+  {lat: 48.4237, lng: -123.1142},
+  {lat: 48.4538, lng: -123.1595},
+  {lat: 48.5493, lng: -123.2185},
+  {lat: 48.6955, lng: -123.2666},
+  {lat: 48.7671, lng: -123.0098},
+  {lat: 48.8331, lng: -123.0084},
+  {lat: 49.0037, lng: -123.3215}
+];
+
+var washShape = new google.maps.Polygon({
+  paths: washCoords,
+  strokeColor: '#595959',
+  strokeOpacity: 0.5,
+  strokeWeight: 2,
+  fillColor: '#595959',
+  fillOpacity: 0.5,
+  cursor: 'grab'
+});
+washShape.setMap(map);
 
 var totalPubStudents = 0;
 var totalPrivStudents = 0;
@@ -265,97 +330,91 @@ var total70complianceSchools = 0;
 var totalbelow70complianceSchools = 0;
 var totalNo6thGraders = 0;
 
-// latlongs.forEach((val,i) => {
-    // if (schools[i][35] === 'Y' && schools[i][36] === 0) {
-      //     totalNo6thGraders += 1;
-      //     var marker = new google.maps.Marker({
-      //       position: {lat: val[1], lng: val[0]},
-      //       icon: 'gray.png',
-      //       map: map,
-      //     });
-    // } else
-//   if (schools[i][35] === 'Y' && schools[i][21] === 1) {
-//     total100complianceSchools += 1;
-//     var marker = new google.maps.Marker({
-//       position: {lat: val[1], lng: val[0]},
-//       icon: 'dark_green.png',
-//       map: map,
-//     });
-//   } else if (schools[i][35] === 'Y' && schools[i][21] >= 0.9) {
-//     total90complianceSchools += 1;
-//     var marker = new google.maps.Marker({
-//       position: {lat: val[1], lng: val[0]},
-//       icon: 'purple.png',
-//       map: map,
-//     });
-//   } else if (schools[i][35] === 'Y' && schools[i][21] >= 0.8) {
-//     total80complianceSchools += 1;
-//     var marker = new google.maps.Marker({
-//       position: {lat: val[1], lng: val[0]},
-//       icon: 'dark_orange.png',
-//       map: map,
-//     });
-//   } else if (schools[i][35] === 'Y' && schools[i][21] >= 0.7) {
-//     total70complianceSchools += 1;
-//     var marker = new google.maps.Marker({
-//       position: {lat: val[1], lng: val[0]},
-//       icon: 'blue.png',
-//       map: map,
-//     });
-//   } else if (schools[i][35] === 'Y') {
-//     totalbelow70complianceSchools += 1;
-//     var marker = new google.maps.Marker({
-//       position: {lat: val[1], lng: val[0]},
-//       icon: 'brown.png',
-//       map: map,
-//     });
-//   } else {
-//     var marker = new google.maps.Marker({
-//       position: {lat: val[1], lng: val[0]},
-//       icon: 'grey_poupon.png',
-//       map: map,
-//     });
-//   }
-
-//   if (schools[i][35] === 'N') {
-//       var marker = new google.maps.Marker({
-//         position: {lat: val[1], lng: val[0]},
-//         icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
-//         map: map,
-//       });
-//       var infowindow = new google.maps.InfoWindow ({ //38 - school name, 1 - city, 2 - county, 4 - grades, 37 - school district 39 - public or private
-//         content: `<dl><dt>School: </dt><dd> ${schools[i][38]}, ${schools[i][1]}</dd><dt>Status:</dt> <dd>Did not report vaccination information.</dd></dl>`,
-//         maxWidth: 500
-//       });
-//   } else if (schools[i][39] === 'PUBLIC SCHOOL') {
-//     var marker = new google.maps.Marker({
-//       position: {lat: val[1], lng: val[0]},
-//       icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-//       map: map,
-//     });
-//     var infowindow = new google.maps.InfoWindow ({ //38 - school name, 1 - city, 2 - county, 4 - grades, 37 - school district 39 - public or private
-//       content: `<dl><dt>School: </dt><dd> ${schools[i][38]}, ${schools[i][1]}</dd><dt>Grades:</dt> <dd>${schools[i][4]}</dd><dt>Number of students:</dt><dd>${schools[i][36]}</dd><dt>% of students with all immunizations:</dt> <dd>${(schools[i][21]*100).toFixed(2)}</dd><dt>% with medical exemption: </dt> <dd>${(schools[i][17]/schools[i][36]*100).toFixed(2)}</dd><dt>% with other exemption:</dt> <dd>${(((schools[i][18]+schools[i][19]+schools[i][20])/schools[i][36])*100).toFixed(2)}</dd></dl>`,
-//       maxWidth: 500
-//     });
-//   }
-//   else {
-//     var marker = new google.maps.Marker({
-//       position: {lat: val[1], lng: val[0]},
-//       icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
-//       map: map,
-//     });
-//     var infowindow = new google.maps.InfoWindow ({ //38 - school name, 1 - city, 2 - county, 4 - grades, 37 - school district 39 - public or private
-//       content: `<dl><dt>School: </dt><dd> ${schools[i][38]}, ${schools[i][1]}</dd><dt>Grades:</dt> <dd>${schools[i][4]}</dd><dt>Number of students:</dt><dd>${schools[i][36]}</dd><dt>% of students with all immunizations:</dt> <dd>${(schools[i][21]*100).toFixed(2)}</dd><dt>% with medical exemption: </dt> <dd>${(schools[i][17]/schools[i][36]*100).toFixed(2)}</dd><dt>% with other exemption:</dt> <dd>${(((schools[i][18]+schools[i][19]+schools[i][20])/schools[i][36])*100).toFixed(2)}</dd></dl>`,
-//       maxWidth: 500
-//     });
-//   }
-//
-//     marker.addListener('click', function() {
-//       if (openWindow) openWindow.close();
-//       infowindow.open(map, marker);
-//       openWindow = infowindow;
-//     });
-// });
+latlongs.forEach((val,i) => {
+    if (schools[i][35] === 'Y' && schools[i][36] === 0) {
+          totalNo6thGraders += 1;
+          var marker = new google.maps.Marker({
+            position: {lat: val[1], lng: val[0]},
+            icon: 'gray.png',
+            map: map,
+          });
+          var infowindow = new google.maps.InfoWindow ({ //38 - school name, 1 - city, 2 - county, 4 - grades, 37 - school district 39 - public or private
+            content: `<dl><dt>School: </dt><dd> ${schools[i][38]}, ${schools[i][1]}</dd><dt>Status:</dt> <dd>No 6th graders enrolled this year at this school.</dd></dl>`,
+            maxWidth: 500
+          });
+    } else
+  if (schools[i][35] === 'Y' && schools[i][21] === 1) {
+    total100complianceSchools += 1;
+    var marker = new google.maps.Marker({
+      position: {lat: val[1], lng: val[0]},
+      icon: 'dark_green.png',
+      map: map,
+    });
+    var infowindow = new google.maps.InfoWindow ({ //38 - school name, 1 - city, 2 - county, 4 - grades, 37 - school district 39 - public or private
+      content: `<dl><dt>School: </dt><dd> ${schools[i][38]}, ${schools[i][1]}</dd><dt>Type:</dt> <dd>${schools[i][39]}</dd><dt>Grades:</dt> <dd>${schools[i][4]}</dd><dt>Number of 6th grade students:</dt><dd>${schools[i][36]}</dd><dt>% of students with all immunizations:</dt> <dd>${(schools[i][21]*100).toFixed(2)}</dd><dt>% with medical exemption: </dt> <dd>${(schools[i][17]/schools[i][36]*100).toFixed(2)}</dd><dt>% with other exemption:</dt> <dd>${(((schools[i][18]+schools[i][19]+schools[i][20])/schools[i][36])*100).toFixed(2)}</dd></dl>`,
+      maxWidth: 500
+    });
+  } else if (schools[i][35] === 'Y' && schools[i][21] >= 0.9) {
+    total90complianceSchools += 1;
+    var marker = new google.maps.Marker({
+      position: {lat: val[1], lng: val[0]},
+      icon: 'purple.png',
+      map: map,
+    });
+    var infowindow = new google.maps.InfoWindow ({ //38 - school name, 1 - city, 2 - county, 4 - grades, 37 - school district 39 - public or private
+      content: `<dl><dt>School: </dt><dd> ${schools[i][38]}, ${schools[i][1]}</dd><dt>Type:</dt> <dd>${schools[i][39]}</dd><dt>Grades:</dt> <dd>${schools[i][4]}</dd><dt>Number of 6th grade students:</dt><dd>${schools[i][36]}</dd><dt>% of students with all immunizations:</dt> <dd>${(schools[i][21]*100).toFixed(2)}</dd><dt>% with medical exemption: </dt> <dd>${(schools[i][17]/schools[i][36]*100).toFixed(2)}</dd><dt>% with other exemption:</dt> <dd>${(((schools[i][18]+schools[i][19]+schools[i][20])/schools[i][36])*100).toFixed(2)}</dd></dl>`,
+      maxWidth: 500
+    });
+  } else if (schools[i][35] === 'Y' && schools[i][21] >= 0.8) {
+    total80complianceSchools += 1;
+    var marker = new google.maps.Marker({
+      position: {lat: val[1], lng: val[0]},
+      icon: 'dark_orange.png',
+      map: map,
+    });
+    var infowindow = new google.maps.InfoWindow ({ //38 - school name, 1 - city, 2 - county, 4 - grades, 37 - school district 39 - public or private
+      content: `<dl><dt>School: </dt><dd> ${schools[i][38]}, ${schools[i][1]}</dd><dt>Type:</dt> <dd>${schools[i][39]}</dd><dt>Grades:</dt> <dd>${schools[i][4]}</dd><dt>Number of 6th grade students:</dt><dd>${schools[i][36]}</dd><dt>% of students with all immunizations:</dt> <dd>${(schools[i][21]*100).toFixed(2)}</dd><dt>% with medical exemption: </dt> <dd>${(schools[i][17]/schools[i][36]*100).toFixed(2)}</dd><dt>% with other exemption:</dt> <dd>${(((schools[i][18]+schools[i][19]+schools[i][20])/schools[i][36])*100).toFixed(2)}</dd></dl>`,
+      maxWidth: 500
+    });
+  } else if (schools[i][35] === 'Y' && schools[i][21] >= 0.7) {
+    total70complianceSchools += 1;
+    var marker = new google.maps.Marker({
+      position: {lat: val[1], lng: val[0]},
+      icon: 'blue.png',
+      map: map,
+    });
+    var infowindow = new google.maps.InfoWindow ({ //38 - school name, 1 - city, 2 - county, 4 - grades, 37 - school district 39 - public or private
+      content: `<dl><dt>School: </dt><dd> ${schools[i][38]}, ${schools[i][1]}</dd><dt>Type:</dt> <dd>${schools[i][39]}</dd><dt>Grades:</dt> <dd>${schools[i][4]}</dd><dt>Number of 6th grade students:</dt><dd>${schools[i][36]}</dd><dt>% of students with all immunizations:</dt> <dd>${(schools[i][21]*100).toFixed(2)}</dd><dt>% with medical exemption: </dt> <dd>${(schools[i][17]/schools[i][36]*100).toFixed(2)}</dd><dt>% with other exemption:</dt> <dd>${(((schools[i][18]+schools[i][19]+schools[i][20])/schools[i][36])*100).toFixed(2)}</dd></dl>`,
+      maxWidth: 500
+    });
+  } else if (schools[i][35] === 'Y') {
+    totalbelow70complianceSchools += 1;
+    var marker = new google.maps.Marker({
+      position: {lat: val[1], lng: val[0]},
+      icon: 'brown.png',
+      map: map,
+    });
+    var infowindow = new google.maps.InfoWindow ({ //38 - school name, 1 - city, 2 - county, 4 - grades, 37 - school district 39 - public or private
+      content: `<dl><dt>School: </dt><dd> ${schools[i][38]}, ${schools[i][1]}</dd><dt>Type:</dt> <dd>${schools[i][39]}</dd><dt>Grades:</dt> <dd>${schools[i][4]}</dd><dt>Number of 6th grade students:</dt><dd>${schools[i][36]}</dd><dt>% of students with all immunizations:</dt> <dd>${(schools[i][21]*100).toFixed(2)}</dd><dt>% with medical exemption: </dt> <dd>${(schools[i][17]/schools[i][36]*100).toFixed(2)}</dd><dt>% with other exemption:</dt> <dd>${(((schools[i][18]+schools[i][19]+schools[i][20])/schools[i][36])*100).toFixed(2)}</dd></dl>`,
+      maxWidth: 500
+    });
+  } else {
+    var marker = new google.maps.Marker({
+      position: {lat: val[1], lng: val[0]},
+      icon: 'grey_poupon.png',
+      map: map,
+    });
+    var infowindow = new google.maps.InfoWindow ({ //38 - school name, 1 - city, 2 - county, 4 - grades, 37 - school district 39 - public or private
+      content: `<dl><dt>School: </dt><dd> ${schools[i][38]}, ${schools[i][1]}</dd><dt>Status:</dt> <dd>Did not report vaccination information.</dd></dl>`,
+      maxWidth: 500
+    });
+  }
+  marker.addListener('click', function() {
+    if (openWindow) openWindow.close();
+    infowindow.open(map, marker);
+    openWindow = infowindow;
+  });
+});
 
 var icons = {
   hundred: {
@@ -388,76 +447,99 @@ var icons = {
   }
 };
 
-// var legend = document.getElementById('legend');
-// for (var key in icons) {
-//   var type = icons[key];
-//   var name = type.name;
-//   var icon = type.icon;
-//   var div = document.createElement('div');
-//   div.innerHTML = '<img src="' + icon + '"> ' + name;
-//   legend.appendChild(div);
-// }
-// map.controls[google.maps.ControlPosition.LEFT_TOP].push(legend);
+var legend = document.getElementById('legend');
+for (var key in icons) {
+  var type = icons[key];
+  var name = type.name;
+  var icon = type.icon;
+  var div = document.createElement('div');
+  div.innerHTML = '<img src="' + icon + '"> ' + name;
+  legend.appendChild(div);
+}
+map.controls[google.maps.ControlPosition.LEFT_TOP].push(legend);
 
 var percentCompliance = schools.filter(school => school[36] !== 0).map(school => school[21]).filter(val => !isNaN(val));
 var numberOfStudents = schools.filter(school => school[36] !== 0).map(school => school[36]).filter(val => !isNaN(val));
 
-var barData = [];
-for (var i = 0; i < 11; i++) barData[i] = 0;
-var totalSchools = 0;
-var aveCompliance = 0;
 
-schools.forEach(val => {
-  if (val[36] !== 0 && val[35] === 'Y') {
-    totalSchools += 1;
-    aveCompliance += val[21];
-    if (val[21] === 1) barData[10] += 1;
-    else if (val[21] > .899) barData[9] += 1;
-    else if (val[21] > .799) barData[8] += 1;
-    else if (val[21] > .699) barData[7] += 1;
-    else if (val[21] > .599) barData[6] += 1;
-    else if (val[21] > .499) barData[5] += 1;
-    else if (val[21] > .399) barData[4] += 1;
-    else if (val[21] > .299) barData[3] += 1;
-    else if (val[21] > .1999) barData[2] += 1;
-    else if (val[21] > .099) barData[1] += 1;
-    else barData[0] += 1;
-  }
-});
 
-var scatterPoints = [];
-var count = 0;
-for (var j = 0; j <= 100; j += 10) {
-  scatterPoints[count] = { x: j, y: barData[count] };
-  console.log(j);
-  count += 1;
-}
-
-aveCompliance = aveCompliance/totalSchools;
-
-var ctx = document.getElementById('scatter').getContext('2d');
-
-var scatterChart = new Chart(ctx, {
+function makeChart(xValue) {
+  var ctx = document.getElementById('scatter').getContext('2d');
+  var scatterChart = new Chart(ctx, {
     type: 'line',
     data: {
-        datasets: [{
-            label: 'Scatter Dataset',
-            data: scatterPoints
+      datasets: [
+        {
+          data: [{x: 77, y: 0}],
+          borderColor: 'yellow',
+          pointRadius: 2,
+          pointStyle: ['line'],
+          borderWidth: 20
         },
         {
-            label: 'Scatter Dataset2',
-            data: [{x: 77, y: 0}]
-        }]
+          data: [{x: xValue, y: 0}],
+          borderColor: 'fuchsia',
+          pointRadius: 2,
+          pointStyle: ['line'],
+          borderWidth: 20
+        },
+        {
+          data: scatterPoints,
+          borderColor: 'gray',
+          pointRadius: 0,
+          borderWidth: 20
+        }
+      ]
     },
     options: {
-        scales: {
-            xAxes: [{
-                type: 'linear',
-                position: 'bottom'
-            }]
-        }
+      padding: 0,
+      legend: {
+        display: false
+      },
+      responsive: false,
+      backgroundColor: 'beige',
+      scales: {
+        xAxes: [{
+          type: 'linear',
+          position: 'bottom',
+          ticks: {
+            fontSize: 13,
+            min: 0,
+            max: 100,
+            fixedStepSize: 20
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          scaleLabel: {
+            display: false,
+            labelString: '% of students in WA with all required immunizations',
+            fontSize: 15
+          }
+        }],
+        yAxes: [{
+          padding: 0,
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          scaleLabel: {
+            display: false
+          },
+          ticks: {
+            display: false,
+            beginAtZero: true,
+            min: 0,
+            max: 0.1,
+            fixedStepSize: 0.1,
+            maxTicksLimit: 0
+          }
+        }]
+      }
     }
-});
+  });
+}
 
 // address:0,
 // city:1,
